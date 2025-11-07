9310caf4409e5ee45fd49b9009e8a0fef3c9bca3
Leven <talalaslam115@gmail.com>
2025-11-07 17:46:08 +0500

    Added Sync of Mulitple Threads using Locks, RLocks, and Semaphores

---
+++ Chapter 2/MyThreadClass_lock.py
@@ -0,0 +1,34 @@
+from do_something import do_something
+import time
+import threading
+
+
+if __name__ == "__main__":
+    size = 10000000
+    counts = [5, 10, 15]
+
+    print("\n--------------------Multi-Threading with Locks--------------------")
+
+    for threads in counts:
+        lock = threading.Lock()
+        jobs = []
+        start_time = time.time()
+
+        def execute_task(size, out_list):
+            with lock:
+                do_something(size, out_list)
+
+        for i in range(threads):
+            out_list = []
+            thread = threading.Thread(target=execute_task, args=(size, out_list))
+            jobs.append(thread)
+
+        for t in jobs:
+            t.start()
+        for t in jobs:
+            t.join()
+
+        end_time = time.time()
+        print(f"Threads: {threads}, Duration = {end_time - start_time:.2f} s")
+
+    print("\n--------------------All threads completed--------------------")
---
+++ Chapter 2/Semaphore.py
@@ -0,0 +1,34 @@
+from do_something import do_something
+import time
+import threading
+
+
+if __name__ == "__main__":
+    size = 10000000
+    counts = [5, 10, 15]
+
+    print("\n--------------------Multi-Threading with Semaphores--------------------")
+
+    for threads in counts:
+        sem = threading.Semaphore()
+        jobs = []
+        start_time = time.time()
+
+        def execute_task(size, out_list):
+            with sem:
+                do_something(size, out_list)
+
+        for i in range(threads):
+            out_list = []
+            thread = threading.Thread(target=execute_task, args=(size, out_list))
+            jobs.append(thread)
+
+        for t in jobs:
+            t.start()
+        for t in jobs:
+            t.join()
+
+        end_time = time.time()
+        print(f"Threads: {threads}, Duration = {end_time - start_time:.2f} s")
+
+    print("\n--------------------All threads completed--------------------")
